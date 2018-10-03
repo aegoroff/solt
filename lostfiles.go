f87c82c4cd2c4e08854c37c2c7c218ee3d411180
egoroff <egoroff@gmail.com>
2018-10-03 09:24:49 +0300

    lostfiles command refactoring (not completed)

--- lostfiles.go
+++ lostfiles.go
@@ -2,42 +2,59 @@ package main
 
 import (
     "fmt"
+    "github.com/anknown/ahocorasick"
     "path/filepath"
     "strings"
 )
 
-var excludeSubFolders = []string{
+var subfolderToExclude = []string{
     "obj",
     "bin",
 }
 
 func lostfilescmd(opt options) error {
-    var includedFiles = make(map[string]interface{})
-    var filteredFiles []string
 
     filter := CSharpCodeFileExt
     if len(opt.LostFiles.Filter) > 0 {
         filter = opt.LostFiles.Filter
     }
 
-    var excludeFolders []string
-
+    var foundFiles []string
     foldersMap := readProjectDir(opt.Path, func(we *walkEntry) {
         // Add file to filtered files slice
         ext := strings.ToLower(filepath.Ext(we.Name))
         if ext == filter {
             fp := filepath.Join(we.Parent, we.Name)
-            filteredFiles = append(filteredFiles, fp)
+            foundFiles = append(foundFiles, fp)
         }
     })
 
+    includedFiles, excludeFoldersMachine := createFilesAndFoldersMap(foldersMap)
+
+    for _, file := range foundFiles {
+        if _, ok := includedFiles[file]; !ok {
+
+            if Match(excludeFoldersMachine, file) {
+                continue
+            }
+
+            fmt.Println(file)
+        }
+    }
+
+    return nil
+}
+
+func createFilesAndFoldersMap(foldersMap map[string]*folderInfo) (map[string]interface{}, *goahocorasick.Machine) {
+    var excludeFolders []string
+    var includedFiles = make(map[string]interface{})
     for k, v := range foldersMap {
         if v.project == nil {
             continue
         }
 
         // Add project base + exlude subfolder into exclude folders list
-        for _, s := range excludeSubFolders {
+        for _, s := range subfolderToExclude {
             sub := filepath.Join(k, s)
             excludeFolders = append(excludeFolders, sub)
         }
@@ -62,19 +79,6 @@ func lostfilescmd(opt options) error {
             }
         }
     }
-
     excludeFoldersMachine := createAhoCorasickMachine(excludeFolders)
-
-    for _, file := range filteredFiles {
-        if _, ok := includedFiles[file]; !ok {
-
-            if Match(excludeFoldersMachine, file) {
-                continue
-            }
-
-            fmt.Println(file)
-        }
-    }
-
-    return nil
+    return includedFiles, excludeFoldersMachine
 }
