d252367a3cb8f0fb7b39ff6706335f57ecde280a
egoroff <egoroff@gmail.com>
2018-10-05 16:24:14 +0300

    Code reorganization

--- domain.go
+++ domain.go
@@ -2,6 +2,18 @@ package main
 
 import "encoding/xml"
 
+type Packages struct {
+    XMLName  xml.Name  `xml:"packages"`
+    Packages []Package `xml:"package"`
+}
+
+type Package struct {
+    Id                    string `xml:"id,attr"`
+    Version               string `xml:"version,attr"`
+    TargetFramework       string `xml:"targetFramework,attr"`
+    DevelopmentDependency string `xml:"developmentDependency,attr"`
+}
+
 type Project struct {
     XMLName           xml.Name           `xml:"Project"`
     Id                string             `xml:"PropertyGroup>ProjectGuid"`
