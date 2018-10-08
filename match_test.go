775102662cd09a4c0b0eea3f3897d28ff7c98981
egoroff <egoroff@gmail.com>
2018-10-08 23:19:14 +0300

    test rewritten

--- match_test.go
+++ match_test.go
@@ -5,41 +5,27 @@ import (
 	"testing"
 )
 
-func Test_SuccessMatchOneOfPatterns_ResultTrue(t *testing.T) {
+func Test_MatchOneOfPatterns(t *testing.T) {
 	// Arrange
 	ass := assert.New(t)
-	patterns := []string{"xxx", "yyy", "zzz"}
-	m := createAhoCorasickMachine(patterns)
+	var tests = []struct {
+		patterns []string
+		input    string
+		result   bool
+	}{
+		{[]string{"xxx", "yyy", "zzz"}, "yyyyy", true},
+		{[]string{"xxx", "yyy", "zzz"}, "yyy", true},
+		{[]string{"xxx", "yyy", "zzz"}, "yyyzzz", true},
+		{[]string{"xxx", "yyy", "zzz"}, "cccyyybbb", true},
+		{[]string{"xxx", "yyy", "zzz"}, "aaa", false},
+	}
 
-	// Act
-	result := Match(m, "yyyyy")
+	for _, test := range tests {
+		// Act
+		m := createAhoCorasickMachine(test.patterns)
+		result := Match(m, test.input)
 
-	// Assert
-	ass.True(result)
-}
-
-func Test_SuccessMatchOneOfPatternsExactly_ResultTrue(t *testing.T) {
-	// Arrange
-	ass := assert.New(t)
-	patterns := []string{"xxx", "yyy", "zzz"}
-	m := createAhoCorasickMachine(patterns)
-
-	// Act
-	result := Match(m, "yyy")
-
-	// Assert
-	ass.True(result)
-}
-
-func Test_NotMatchAnyOfPatterns_ResultFalse(t *testing.T) {
-	// Arrange
-	ass := assert.New(t)
-	patterns := []string{"xxx", "yyy", "zzz"}
-	m := createAhoCorasickMachine(patterns)
-
-	// Act
-	result := Match(m, "aaa")
-
-	// Assert
-	ass.False(result)
+		// Assert
+		ass.Equal(test.result, result)
+	}
 }
