b7af548f4b30ca9d8337ca6a27020751439db7e3
egoroff <egoroff@gmail.com>
2018-10-10 08:23:00 +0300

    Potential case sensitivity problem when finding lost projects fixed

--- lostprojectscmd.go
+++ lostprojectscmd.go
@@ -42,15 +42,14 @@ func getOutsideProjectsAndFilesInsideSolution(folders []*folderInfo, allProjects
 		if info.project == nil {
 			continue
 		}
-		project := *info.projectPath
-		_, ok := allProjectsWithinSolutions[project]
+		_, ok := allProjectsWithinSolutions[info.project.Id]
 		if !ok {
 			projectsOutsideSolution = append(projectsOutsideSolution, info)
 		} else {
 			filesIncluded := getFilesIncludedIntoProject(info)
 
 			for _, f := range filesIncluded {
-				filesInsideSolution[f] = nil
+				filesInsideSolution[strings.ToUpper(f)] = nil
 			}
 		}
 	}
@@ -61,15 +60,15 @@ func separateOutsideProjects(projectsOutsideSolution []*folderInfo, filesInsideS
 	var projectsOutside []string
 	var projectsOutsideSolutionWithFilesInside []string
 	for _, info := range projectsOutsideSolution {
-		filesIncluded := getFilesIncludedIntoProject(info)
+		projectFiles := getFilesIncludedIntoProject(info)
 
 		var includedIntoOther = false
-		for _, f := range filesIncluded {
-			if _, ok := filesInsideSolution[f]; ok {
+		for _, f := range projectFiles {
+			if _, ok := filesInsideSolution[strings.ToUpper(f)]; ok {
 
 				dir := filepath.Dir(*info.projectPath)
 
-				if strings.Contains(f, dir) {
+				if strings.Contains(strings.ToUpper(f), strings.ToUpper(dir)) {
 					includedIntoOther = true
 					break
 				}
@@ -90,18 +89,14 @@ func getAllSolutionsProjects(solutions []string) map[string]interface{} {
 	for _, solpath := range solutions {
 		sln, _ := solution.Parse(solpath)
 
-		parent := filepath.Dir(solpath)
-
 		for _, p := range sln.Projects {
 			// Skip solution folders
 			if p.TypeId == "{2150E333-8FDC-42A3-9474-1A3956D46DE8}" {
 				continue
 			}
 
-			pp := filepath.Join(parent, p.Path)
-
-			if _, ok := projectsInSolution[pp]; !ok {
-				projectsInSolution[pp] = nil
+			if _, ok := projectsInSolution[p.Id]; !ok {
+				projectsInSolution[p.Id] = nil
 			}
 		}
 	}
