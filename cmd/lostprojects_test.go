dee402abefc73a93132a45fbdf6988e439e234ef
egoroff <egoroff@gmail.com>
2020-05-18 21:10:59 +0300

    more projects tests

--- cmd/lostprojects_test.go
+++ cmd/lostprojects_test.go
@@ -33,7 +33,7 @@ func Test_FindLostProjectsCmd_NoLostProjectsFound(t *testing.T) {
 	ass.Equal(``, actual)
 }
 
-func Test_FindLostProjectsCmd_LostProjectsFound(t *testing.T) {
+func Test_FindLostProjectsCmdLostProjectsInTheSameDir_LostProjectsFound(t *testing.T) {
 	// Arrange
 	ass := assert.New(t)
 	dir := "a/"
@@ -61,5 +61,63 @@ func Test_FindLostProjectsCmd_LostProjectsFound(t *testing.T) {
 These projects are not included into any solution but files from the projects' folders are used in another projects within a solution:
 
  a\a\a1.csproj
+`, actual)
+}
+
+func Test_FindLostProjectsCmdLostProjectsInOtherDir_LostProjectsFound(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	dir := "a/"
+	memfs := afero.NewMemMapFs()
+	memfs.MkdirAll(dir + "a/Properties", 0755)
+	afero.WriteFile(memfs, dir+"a.sln", []byte(testSolutionContent), 0644)
+	afero.WriteFile(memfs, dir+"a/a.csproj", []byte(testProjectContent), 0644)
+	afero.WriteFile(memfs, dir+"a1/a1.csproj", []byte(testProjectContent2), 0644)
+	afero.WriteFile(memfs, dir+"a/App.config", []byte(appConfigContent), 0644)
+	afero.WriteFile(memfs, dir+"a/Program.cs", []byte(codeFileContent), 0644)
+	afero.WriteFile(memfs, dir+"a/Properties/AssemblyInfo.cs", []byte(assemblyInfoContent), 0644)
+
+	buf := bytes.NewBufferString("")
+
+	appWriter = buf
+	appFileSystem = memfs
+
+	// Act
+	rootCmd.SetArgs([]string{"lp", "-p", dir})
+	rootCmd.Execute()
+
+	// Assert
+	actual := buf.String();
+	ass.Equal(` a\a1\a1.csproj
+`, actual)
+}
+
+func Test_FindLostProjectsCmdUnexistProjects_LostProjectsFound(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	dir := "a/"
+	memfs := afero.NewMemMapFs()
+	memfs.MkdirAll(dir + "a/Properties", 0755)
+	afero.WriteFile(memfs, dir+"a.sln", []byte(testSolutionContent), 0644)
+	afero.WriteFile(memfs, dir+"a/App.config", []byte(appConfigContent), 0644)
+	afero.WriteFile(memfs, dir+"a/Program.cs", []byte(codeFileContent), 0644)
+	afero.WriteFile(memfs, dir+"a/Properties/AssemblyInfo.cs", []byte(assemblyInfoContent), 0644)
+
+	buf := bytes.NewBufferString("")
+
+	appWriter = buf
+	appFileSystem = memfs
+
+	// Act
+	rootCmd.SetArgs([]string{"lp", "-p", dir})
+	rootCmd.Execute()
+
+	// Assert
+	actual := buf.String();
+	ass.Equal(`
+These projects are included into a solution but not found in the file system:
+
+Solution: a\a.sln
+ a\a\a.csproj
 `, actual)
 }
