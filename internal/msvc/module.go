70e9cb1045b628395017dd9e233447450bbfed96
egoroff <egoroff@gmail.com>
2020-06-03 13:22:48 +0300

    modules code moved to module.go file

---
+++ internal/msvc/module.go
@@ -0,0 +1,145 @@
+package msvc
+
+import (
+	"github.com/spf13/afero"
+	"log"
+	"path/filepath"
+	"solt/internal/sys"
+	"solt/solution"
+	"strings"
+)
+
+type readerHandler interface {
+	handler(path string)
+}
+
+type readerModule interface {
+	filter(path string) bool
+	read(path string) (*Folder, bool)
+}
+
+type readerModules struct {
+	modules    []readerModule
+	aggregator chan *Folder
+}
+
+type readerPackagesConfig struct {
+	fs afero.Fs
+}
+
+type readerMsbuild struct {
+	fs afero.Fs
+}
+
+type readerSolution struct {
+	fs afero.Fs
+}
+
+func (r *readerModules) handler(path string) {
+	for _, m := range r.modules {
+		if !m.filter(path) {
+			continue
+		}
+		if folder, ok := m.read(path); ok {
+			r.aggregator <- folder
+		}
+	}
+}
+
+// packages.config
+
+func (r *readerPackagesConfig) filter(path string) bool {
+	_, file := filepath.Split(path)
+	return strings.EqualFold(file, packagesConfigFile)
+}
+
+func (r *readerPackagesConfig) read(path string) (*Folder, bool) {
+	pack := packages{}
+
+	err := onXMLFile(path, r.fs, &pack)
+	if err != nil {
+		return nil, false
+	}
+
+	f := createFolder(path)
+
+	f.Content.Packages = &pack
+
+	return f, true
+}
+
+// MSBuild projects
+
+func (r *readerMsbuild) filter(path string) bool {
+	ext := filepath.Ext(path)
+	return strings.EqualFold(ext, csharpProjectExt) || strings.EqualFold(ext, cppProjectExt)
+}
+
+func (r *readerMsbuild) read(path string) (*Folder, bool) {
+	project := msbuildProject{}
+
+	err := onXMLFile(path, r.fs, &project)
+	if err != nil {
+		return nil, false
+	}
+
+	f := createFolder(path)
+
+	p := MsbuildProject{Project: &project, Path: path}
+
+	f.Content.Projects = append(f.Content.Projects, &p)
+
+	return f, true
+}
+
+// VS Solutions
+
+func (r *readerSolution) filter(path string) bool {
+	ext := filepath.Ext(path)
+	return strings.EqualFold(ext, solutionFileExt)
+}
+
+func (r *readerSolution) read(path string) (*Folder, bool) {
+	reader, err := r.fs.Open(filepath.Clean(path))
+	if err != nil {
+		log.Println(err)
+		return nil, false
+	}
+
+	sln, err := solution.Parse(reader)
+
+	if err != nil {
+		log.Println(err)
+		return nil, false
+	}
+
+	f := createFolder(path)
+
+	s := VisualStudioSolution{Solution: sln, Path: path}
+
+	f.Content.Solutions = append(f.Content.Solutions, &s)
+
+	return f, true
+}
+
+func createFolder(path string) *Folder {
+	f := Folder{
+		Content: &FolderContent{
+			Solutions: []*VisualStudioSolution{},
+			Projects:  []*MsbuildProject{},
+		},
+		Path: filepath.Dir(path),
+	}
+	return &f
+}
+
+func onXMLFile(path string, fs afero.Fs, result interface{}) error {
+
+	err := sys.UnmarshalXMLFrom(path, fs, result)
+	if err != nil {
+		log.Printf("%s: %v\n", path, err)
+		return err
+	}
+
+	return nil
+}
--- internal/msvc/reader.go
+++ internal/msvc/reader.go
@@ -4,7 +4,6 @@ import (
 	"github.com/aegoroff/godatastruct/collections"
 	"github.com/aegoroff/godatastruct/rbtree"
 	"github.com/spf13/afero"
-	"log"
 	"path/filepath"
 	"solt/internal/sys"
 	"solt/solution"
@@ -26,15 +25,6 @@ type VisualStudioSolution struct {
 
 	// filesystem pa
 	Path string
-}
-
-type readerHandler interface {
-	handler(path string)
-}
-
-type readerModule interface {
-	filter(path string) bool
-	read(path string) (*Folder, bool)
 }
 
 // SelectAllSolutionProjectPaths gets all possible projects' paths defined in solution
@@ -154,127 +144,4 @@ func ReadSolutionDir(path string, fs afero.Fs, action func(path string)) rbtree.
 	wg.Wait()
 
 	return result
-}
-
-type readerModules struct {
-	modules    []readerModule
-	aggregator chan *Folder
-}
-
-type readerPackagesConfig struct {
-	fs afero.Fs
-}
-
-type readerMsbuild struct {
-	fs afero.Fs
-}
-
-type readerSolution struct {
-	fs afero.Fs
-}
-
-func (r *readerModules) handler(path string) {
-	for _, m := range r.modules {
-		if !m.filter(path) {
-			continue
-		}
-		if folder, ok := m.read(path); ok {
-			r.aggregator <- folder
-		}
-	}
-}
-
-func (r *readerPackagesConfig) filter(path string) bool {
-	_, file := filepath.Split(path)
-	return strings.EqualFold(file, packagesConfigFile)
-}
-
-// Create packages model from packages.config
-func (r *readerPackagesConfig) read(path string) (*Folder, bool) {
-	pack := packages{}
-
-	err := onXMLFile(path, r.fs, &pack)
-	if err != nil {
-		return nil, false
-	}
-
-	f := createFolder(path)
-
-	f.Content.Packages = &pack
-
-	return f, true
-}
-
-func (r *readerMsbuild) filter(path string) bool {
-	ext := filepath.Ext(path)
-	return strings.EqualFold(ext, csharpProjectExt) || strings.EqualFold(ext, cppProjectExt)
-}
-
-// Create project model from project file
-func (r *readerMsbuild) read(path string) (*Folder, bool) {
-	project := msbuildProject{}
-
-	err := onXMLFile(path, r.fs, &project)
-	if err != nil {
-		return nil, false
-	}
-
-	f := createFolder(path)
-
-	p := MsbuildProject{Project: &project, Path: path}
-
-	f.Content.Projects = append(f.Content.Projects, &p)
-
-	return f, true
-}
-
-func (r *readerSolution) filter(path string) bool {
-	ext := filepath.Ext(path)
-	return strings.EqualFold(ext, solutionFileExt)
-}
-
-// Create solution model from file
-func (r *readerSolution) read(path string) (*Folder, bool) {
-	reader, err := r.fs.Open(filepath.Clean(path))
-	if err != nil {
-		log.Println(err)
-		return nil, false
-	}
-
-	sln, err := solution.Parse(reader)
-
-	if err != nil {
-		log.Println(err)
-		return nil, false
-	}
-
-	f := createFolder(path)
-
-	s := VisualStudioSolution{Solution: sln, Path: path}
-
-	f.Content.Solutions = append(f.Content.Solutions, &s)
-
-	return f, true
-}
-
-func createFolder(path string) *Folder {
-	f := Folder{
-		Content: &FolderContent{
-			Solutions: []*VisualStudioSolution{},
-			Projects:  []*MsbuildProject{},
-		},
-		Path: filepath.Dir(path),
-	}
-	return &f
-}
-
-func onXMLFile(path string, fs afero.Fs, result interface{}) error {
-
-	err := sys.UnmarshalXMLFrom(path, fs, result)
-	if err != nil {
-		log.Printf("%s: %v\n", path, err)
-		return err
-	}
-
-	return nil
 }
