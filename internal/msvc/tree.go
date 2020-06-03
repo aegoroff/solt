01f1fd121a05a1e48ba649af6bdf3cc1bcc0b6c5
egoroff <egoroff@gmail.com>
2020-06-03 20:42:17 +0300

    internal refactoring

--- internal/msvc/tree.go
+++ internal/msvc/tree.go
@@ -37,33 +37,57 @@ func (x *Folder) String() string {
 
 // WalkProjects traverse all projects found in solution(s) folder
 func WalkProjects(foldersTree rbtree.RbTree, action func(prj *MsbuildProject, fold *Folder)) {
-	foldersTree.WalkInorder(func(n rbtree.Node) {
-		fold := n.Key().(*Folder)
-		content := fold.Content
-		if len(content.Projects) == 0 {
-			return
-		}
-
-		// All found projects
-		for _, prj := range content.Projects {
-			action(prj, fold)
-		}
-	})
+	w := &walkP{handler: action}
+	walk(foldersTree, w)
 }
 
 // SelectSolutions gets all Visual Studion solutions found in a directory
 func SelectSolutions(foldersTree rbtree.RbTree) []*VisualStudioSolution {
-	var solutions []*VisualStudioSolution
+	w := walkS{solutions: make([]*VisualStudioSolution, 0)}
+	walk(foldersTree, &w)
+	return w.solutions
+}
+
+type walker interface {
+	onFolder(f *Folder)
+}
+
+type ProjectHandler func(prj *MsbuildProject, fold *Folder)
+
+type walkP struct {
+	handler ProjectHandler
+}
+
+type walkS struct {
+	solutions []*VisualStudioSolution
+}
+
+func (w *walkP) onFolder(f *Folder) {
+	content := f.Content
+	if len(content.Projects) == 0 {
+		return
+	}
+
+	// All found projects
+	for _, prj := range content.Projects {
+		w.handler(prj, f)
+	}
+}
+
+func (w *walkS) onFolder(f *Folder) {
+	content := f.Content
 	// Select only folders that contain solution(s)
+	if len(content.Solutions) == 0 {
+		return
+	}
+	for _, sln := range content.Solutions {
+		w.solutions = append(w.solutions, sln)
+	}
+}
+
+func walk(foldersTree rbtree.RbTree, w walker) {
 	foldersTree.WalkInorder(func(n rbtree.Node) {
-		f := n.Key().(*Folder)
-		content := f.Content
-		if len(content.Solutions) == 0 {
-			return
-		}
-		for _, sln := range content.Solutions {
-			solutions = append(solutions, sln)
-		}
+		fold := n.Key().(*Folder)
+		w.onFolder(fold)
 	})
-	return solutions
 }
