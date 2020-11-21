a18e5020e5eef1a080daf5938919d2f335208585
egoroff <egoroff@gmail.com>
2020-11-21 23:46:56 +0300

    refactoring: subfoldersToExclude moved to initialize method

--- cmd/lostfiles_logic.go
+++ cmd/lostfiles_logic.go
@@ -8,39 +8,39 @@ import (
 )
 
 type lostFilesLogic struct {
-	foundFiles          []string
-	excludeFolders      c9s.StringHashSet
-	unexistFiles        map[string][]string
-	includedFiles       c9s.StringHashSet
-	subfoldersToExclude []string
-	nonExistence        bool
-	filer               sys.Filer
+	foundFiles     []string
+	excludeFolders c9s.StringHashSet
+	unexistFiles   map[string][]string
+	includedFiles  c9s.StringHashSet
+	nonExistence   bool
+	filer          sys.Filer
 }
 
 func newLostFilesLogic(nonExistence bool, foundFiles []string, foldersToIgnore c9s.StringHashSet, filer sys.Filer) *lostFilesLogic {
 	return &lostFilesLogic{
-		foundFiles:          foundFiles,
-		excludeFolders:      foldersToIgnore,
-		unexistFiles:        make(map[string][]string),
-		includedFiles:       make(c9s.StringHashSet),
-		subfoldersToExclude: []string{"obj"},
-		nonExistence:        nonExistence,
-		filer:               filer,
+		foundFiles:     foundFiles,
+		excludeFolders: foldersToIgnore,
+		unexistFiles:   make(map[string][]string),
+		includedFiles:  make(c9s.StringHashSet),
+		nonExistence:   nonExistence,
+		filer:          filer,
 	}
 }
 
 // initialize fills subfoldersToExclude, excludeFolders, includedFiles and unexistFiles
 func (h *lostFilesLogic) initialize(projects []*msvc.MsbuildProject) {
+	subfoldersToExclude := []string{"obj"}
+
 	for _, prj := range projects {
 		pdir := filepath.Dir(prj.Path)
 
 		// Exclude output paths too
 		if prj.Project.OutputPaths != nil {
-			h.subfoldersToExclude = append(h.subfoldersToExclude, prj.Project.OutputPaths...)
+			subfoldersToExclude = append(subfoldersToExclude, prj.Project.OutputPaths...)
 		}
 
 		// Add project base + exclude subfolder into exclude folders list
-		for _, s := range h.subfoldersToExclude {
+		for _, s := range subfoldersToExclude {
 			sub := filepath.Join(pdir, s)
 			h.excludeFolders.Add(sub)
 		}
