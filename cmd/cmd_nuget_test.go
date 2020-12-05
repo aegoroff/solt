bc7ed4f134f724530bed35e7eecc825a2819b51d
egoroff <egoroff@gmail.com>
2020-12-05 13:58:11 +0300

    One more nuget command test

--- cmd/cmd_nuget_test.go
+++ cmd/cmd_nuget_test.go
@@ -36,6 +36,35 @@ func Test_NugetCmd_OutputAsSpecified(t *testing.T) {
 `, actual)
 }
 
+func Test_NugetCmdOnSdkProjects_OutputAsSpecified(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	dir := "a/"
+	memfs := afero.NewMemMapFs()
+	afero.WriteFile(memfs, dir+"a.sln", []byte(coreSolutionContent), 0644)
+	afero.WriteFile(memfs, dir+"a/a.csproj", []byte(aSdkProjectContent), 0644)
+	afero.WriteFile(memfs, dir+"a/Program.cs", []byte(codeFileContent), 0644)
+	afero.WriteFile(memfs, dir+"b/b.csproj", []byte(bSdkProjectContent), 0644)
+	afero.WriteFile(memfs, dir+"b/Class1.cs", []byte(codeFileContent), 0644)
+	afero.WriteFile(memfs, dir+"c/c.csproj", []byte(cSdkProjectContent), 0644)
+	afero.WriteFile(memfs, dir+"c/Class1.cs", []byte(codeFileContent), 0644)
+
+	appPrinter = newMockPrn()
+	appFileSystem = memfs
+
+	// Act
+	Execute("nu", "-p", dir, "-r")
+
+	// Assert
+	actual := appPrinter.(*mockprn).String()
+	ass.Equal(`
+ a\a
+  Package              Version
+  -------              -------
+  CommandLineParser    2.8.0
+`, actual)
+}
+
 func Test_NugetCmdFindMismatch_OutputAsSpecified(t *testing.T) {
 	// Arrange
 	ass := assert.New(t)
