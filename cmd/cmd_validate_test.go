9cbd0dc15e82f143812d0392ba209d35adba8ab9
egoroff <egoroff@gmail.com>
2020-12-18 21:46:44 +0300

    one more validation test added

--- cmd/cmd_validate_test.go
+++ cmd/cmd_validate_test.go
@@ -32,6 +32,32 @@ func Test_ValidateSdkSolutionCmd_RedundantReferencesFound(t *testing.T) {
 `, actual)
 }
 
+func Test_ValidateSdkSolutionCmdWithRefsRemove_RedundantReferencesFound(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	dir := "a/"
+	memfs := afero.NewMemMapFs()
+	_ = afero.WriteFile(memfs, dir+"a.sln", []byte(coreSolutionContent), 0644)
+	_ = afero.WriteFile(memfs, dir+"a/a.csproj", []byte(aSdkProjectContent), 0644)
+	_ = afero.WriteFile(memfs, dir+"a/Program.cs", []byte(codeFileContent), 0644)
+	_ = afero.WriteFile(memfs, dir+"b/b.csproj", []byte(bSdkProjectContent), 0644)
+	_ = afero.WriteFile(memfs, dir+"b/Class1.cs", []byte(codeFileContent), 0644)
+	_ = afero.WriteFile(memfs, dir+"c/c.csproj", []byte(cSdkProjectContent), 0644)
+	_ = afero.WriteFile(memfs, dir+"c/Class1.cs", []byte(codeFileContent), 0644)
+
+	p := newMockPrn()
+
+	// Act
+	_ = execute(memfs, p, "va", "-p", dir, "-r")
+
+	// Assert
+	actual := p.w.String()
+	ass.Equal(` Solution: <green>a\a.sln</>
+   project: <bold>a\a\a.csproj</> has redundant references
+     <gray>a\b\b.csproj</>
+`, actual)
+}
+
 func Test_ValidateOldSolutionCmd_RedundantReferencesNotFound(t *testing.T) {
 	// Arrange
 	ass := assert.New(t)
