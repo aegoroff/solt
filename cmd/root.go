6f60f1db8670b31da538fe65dd5470631a4a386d
egoroff <egoroff@gmail.com>
2020-12-21 21:44:47 +0300

    all content of util.go moved to root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"github.com/dustin/go-humanize"
 	"github.com/spf13/afero"
 	"io"
+	"runtime"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -61,4 +63,16 @@ func execute(fs afero.Fs, p printer, args ...string) error {
 
 func init() {
 	cobra.MousetrapHelpText = ""
+}
+
+// printMemUsage outputs the current, total and OS memory being used. As well as the number
+// of garage collection cycles completed.
+func printMemUsage(p printer) {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	p.cprint("\n<gray>Alloc =</> <green>%s</>", humanize.IBytes(m.Alloc))
+	p.cprint("\t<gray>TotalAlloc =</> <green>%s</>", humanize.IBytes(m.TotalAlloc))
+	p.cprint("\t<gray>Sys =</> <green>%s</>", humanize.IBytes(m.Sys))
+	p.cprint("\t<gray>NumGC =</> <green>%v</>\n", m.NumGC)
 }
