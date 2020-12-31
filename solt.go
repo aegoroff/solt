22f74fb42467e6abb794c552417f8c7d5af71f41
egoroff <egoroff@gmail.com>
2020-12-31 15:06:47 +0300

    refactoring: console environment code moved to console.go

--- solt.go
+++ solt.go
@@ -1,34 +1,13 @@
 package main
 
 import (
-	"github.com/gookit/color"
 	"github.com/spf13/afero"
-	"io"
 	"os"
 	"solt/cmd"
-	"solt/cmd/api"
 )
 
 func main() {
 	if err := cmd.Execute(afero.NewOsFs(), newConsoleEnvironment()); err != nil {
 		os.Exit(1)
 	}
-}
-
-type consoleEnvironment struct{}
-
-func newConsoleEnvironment() api.PrintEnvironment {
-	return &consoleEnvironment{}
-}
-
-func (e *consoleEnvironment) NewPrinter() (api.Printer, error) {
-	return api.NewPrinter(e), nil
-}
-
-func (*consoleEnvironment) PrintFunc(w io.Writer, format string, a ...interface{}) {
-	color.Fprintf(w, format, a...)
-}
-
-func (*consoleEnvironment) Writer() io.WriteCloser {
-	return os.Stdout
 }
