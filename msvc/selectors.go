c30923abf63d99658fc8d122b655a0387b6adaf2
egoroff <egoroff@gmail.com>
2021-01-08 23:25:02 +0300

    refactoring: walkers.go merged to selectors.go

--- msvc/selectors.go
+++ msvc/selectors.go
@@ -40,3 +40,43 @@ func WalkProjectFolders(foldersTree rbtree.RbTree, action ProjectHandler) {
 	w := &walkPrj{handler: action}
 	walk(foldersTree, w)
 }
+
+type walkPrj struct {
+	handler ProjectHandler
+}
+
+func (w *walkPrj) walk(f *Folder) {
+	content := f.Content
+
+	if len(content.Projects) == 0 {
+		return
+	}
+
+	// All found projects
+	for _, prj := range content.Projects {
+		w.handler(prj, f)
+	}
+}
+
+type walkSol struct {
+	solutions []*VisualStudioSolution
+}
+
+func (w *walkSol) walk(f *Folder) {
+	content := f.Content
+	// Select only folders that contain solution(s)
+	if len(content.Solutions) == 0 {
+		return
+	}
+
+	w.solutions = append(w.solutions, content.Solutions...)
+}
+
+func walk(foldersTree rbtree.RbTree, walkers ...walker) {
+	rbtree.NewWalkInorder(foldersTree).Foreach(func(n rbtree.Comparable) {
+		fold := n.(*Folder)
+		for _, w := range walkers {
+			w.walk(fold)
+		}
+	})
+}
